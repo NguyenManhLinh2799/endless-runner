c536bee478e5e6b0ef593b2f03e27210ca360183
Nguyen Manh Linh <nmlinh2799@gmail.com>
2020-03-19 23:17:29 +0700

    Initial commit

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    public int damage = 1;
+    public float speed;
+    public GameObject effect;
+    private Animator camAnim;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        camAnim = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player")) // Player takes damage!
+        {
+            camAnim.SetTrigger("shake");
+            Instantiate(effect, transform.position, Quaternion.identity); // Hieu ung no tung!
+
+            other.GetComponent<Player>().health -= damage;
+            Debug.Log(other.GetComponent<Player>().health);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    private Vector2 targetPos; // Vi tri di chuyen toi
+    public float Yincrement; // Bien do dao dong
+
+    public float speed; // Toc do dao dong
+    public float maxHeight;
+    public float minHeight;
+    public int health = 3;
+
+    public GameObject effect;
+    public Text healthDisplay;
+
+    public GameObject gameStart;
+    public GameObject gameOver;
+    public GameObject spawner;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthDisplay.text = "HP: " + health.ToString();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            gameStart.SetActive(false);
+            spawner.SetActive(true);
+        }
+
+        if (health <= 0) // Het mau!
+        {
+            gameOver.SetActive(true);
+            Destroy(gameObject);
+        }
+
+        transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) && transform.position.y < maxHeight)
+        {
+            targetPos = new Vector2(transform.position.x, transform.position.y + Yincrement);
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow) && transform.position.y > minHeight)
+        {
+            targetPos = new Vector2(transform.position.x, transform.position.y - Yincrement);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Instantiate(effect, transform.position, Quaternion.identity);
+    }
+}
